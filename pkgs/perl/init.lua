98506552124dab5ca666e1d2e15fffde8a2d3987
Stilic <lateman-jpeg@outlook.fr>
2025-04-24 00:11:20 +0200

    Update init.lua

--- pkgs/perl/init.lua
+++ pkgs/perl/init.lua
@@ -12,9 +12,8 @@ self.sources = {
 function self.build()
     lfs.chdir("source")
     os.execute('./Configure -des -Dprefix="' ..
-        lfs.currentdir() .. '/_install/usr"')
-    os.execute('make CFLAGS="-DNO_POSIX_2008_LOCALE -D_GNU_SOURCE" LDFLAGS="-Wl,-z,stack-size=2097152"' ..
-    system.get_make_jobs())
+        lfs.currentdir() .. '/_install/usr" CCFLAGS="-DNO_POSIX_2008_LOCALE -D_GNU_SOURCE"')
+    os.execute("make" .. system.get_make_jobs())
     os.execute("make install")
 end
 
