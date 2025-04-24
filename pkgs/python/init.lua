3afbaaf589a485a3c489a01e48c7cfc4ee5a0aa3
Stilic <lateman-jpeg@outlook.fr>
2025-04-25 01:22:33 +0200

    Update init.lua

--- pkgs/python/init.lua
+++ pkgs/python/init.lua
@@ -12,7 +12,7 @@ self.sources = {
 function self.build()
     lfs.chdir("source")
     os.execute(
-    './configure CFLAGS="-O2 -D_XOPEN_SOURCE=700 -I/usr/include" --prefix=/usr --enable-shared --with-system-expat')
+        './configure CFLAGS="-lcurses" --prefix=/usr --enable-shared --with-system-expat')
     os.execute("make" .. system.get_make_jobs())
     lfs.mkdir("_install")
     os.execute('make install DESTDIR="' .. lfs.currentdir() .. '/_install"')
