176f8e03fea630435ab1e55b034281685cb7a94a
Stilic <lateman-jpeg@outlook.fr>
2025-04-24 13:41:19 +0200

    Update init.lua

--- pkgs/libxml2/init.lua
+++ pkgs/libxml2/init.lua
@@ -11,7 +11,7 @@ self.sources = {
 
 function self.build()
     lfs.chdir("source")
-    os.execute('./configure CFLAGS="-O2" --prefix=/usr')
+    os.execute('./configure CFLAGS="-O2" --prefix=/usr --enable-static --with-legacy --with-zlib')
     os.execute("make" .. system.get_make_jobs())
     lfs.mkdir("_install")
     os.execute('make install DESTDIR="' .. lfs.currentdir() .. '/_install"')
@@ -19,6 +19,10 @@ end
 
 function self.pack()
     os.execute("cp -ra source/_install/* filesystem")
+
+    local file = "libxml2.so.16.0.2"
+    lfs.link(file, "filesystem/usr/lib/libxml2.so." .. self.version:sub(1, 1), true)
+    lfs.link(file, "filesystem/usr/lib/libxml2.so." .. self.version, true)
 end
 
 return self
