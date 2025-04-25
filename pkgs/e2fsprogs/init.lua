05a816cff52a6b79648013ea399493a460e6bb7a
Stilic <lateman-jpeg@outlook.fr>
2025-04-25 10:06:11 +0200

    neld: add e2fsprogs

---
+++ pkgs/e2fsprogs/init.lua
@@ -0,0 +1,25 @@
+local lfs = require "lfs"
+local system = require "system"
+
+local self = {}
+
+self.version = "1.47.1"
+
+self.sources = {
+    { "source", "https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v" ..
+    self.version .. "/e2fsprogs-" .. self.version .. ".tar.xz" }
+}
+
+function self.build()
+    lfs.chdir("source")
+    os.execute('./configure CFLAGS="-O2" --prefix=/usr')
+    os.execute("make" .. system.get_make_jobs())
+    lfs.mkdir("_install")
+    os.execute('make install DESTDIR="' .. lfs.currentdir() .. '/_install"')
+end
+
+function self.pack()
+    os.execute("cp -ra source/_install/* filesystem")
+end
+
+return self
