1942dc59ce7135e641adc2012d59b777cc622ee5
Stilic <lateman-jpeg@outlook.fr>
2025-04-26 23:54:28 +0200

    Update init.lua

--- pkgs/util-linux/init.lua
+++ pkgs/util-linux/init.lua
@@ -7,7 +7,7 @@ self.sources = {
     { "source", "https://www.kernel.org/pub/linux/utils/util-linux/v" .. self.version .. "/util-linux-" .. self.version .. ".tar.xz" }
 }
 
-self.build = tools.build_gnu_configure("")
+self.build = tools.build_gnu_configure("", "--without-ncursesw")
 self.pack = tools.pack_default()
 
 return self
