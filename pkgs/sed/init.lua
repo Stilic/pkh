2a082b996a8c601980077c3047195b94f065b949
Stilic <lateman-jpeg@outlook.fr>
2025-04-26 08:42:27 +0200

    pkgs: add sed

---
+++ pkgs/sed/init.lua
@@ -0,0 +1,13 @@
+local tools = require "tools"
+
+local self = {}
+
+self.version = "4.9"
+self.sources = {
+    { "source", "https://ftp.gnu.org/gnu/sed/sed-" .. self.version .. ".tar.xz" }
+}
+
+self.build = tools.build_gnu_configure()
+self.pack = tools.pack_default()
+
+return self
