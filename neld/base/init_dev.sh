8d75469babc425f1c7b584d6046a16a4b359c9e6
Stilic <lateman-jpeg@outlook.fr>
2025-07-06 13:40:28 +0200

    fix login prompt

--- neld/base/init_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PATH=/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin
-export ARCH=x86
-export TERM=xterm
-luarocks install luafilesystem
--- neld/build_disk.sh
+++ neld/build_disk.sh
@@ -7,7 +7,7 @@ fi
 truncate -s 100g disk
 mkfs.ext4 -F disk
 mount disk /mnt
-mkdir -p /mnt/proc /mnt/sys /mnt/dev /mnt/run /mnt/mnt /mnt/etc /mnt/lib /mnt/sbin /mnt/bin /mnt/tmp /mnt/usr /mnt/run /mnt/var/run /mnt/var/log/init
+mkdir -p /mnt/proc /mnt/sys /mnt/dev /mnt/mnt /mnt/root /mnt/etc /mnt/lib /mnt/sbin /mnt/bin /mnt/tmp /mnt/usr /mnt/run /mnt/var/run /mnt/var/log/init
 cp -ra root/* /mnt
 cp -ra base/* /mnt
 umount /mnt
