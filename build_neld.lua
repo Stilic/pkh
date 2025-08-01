a1fb13b18e5d438f6192bbf19188d1d15b9e9c2a
Stilic <lateman-jpeg@outlook.fr>
2025-08-02 00:54:32 +0200

    fix this crap?

--- build_neld.lua
+++ build_neld.lua
@@ -1,5 +1,11 @@
-local config = require "neld.config"
+local lfs = require "lfs"
 local pkh = require "main"
+local config = require "neld.config"
+
+local current_directory = lfs.currentdir()
+package.path = package.path
+    .. ";" .. current_directory .. "/?.lua"
+    .. ";" .. current_directory .. "/?/init.lua"
 
 for name, layer in pairs(config.layers) do
     for _, package in ipairs(layer) do
