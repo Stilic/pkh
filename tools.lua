0783bd3cdce91c45c38bd4bffa492f590d035093
Stilic <lateman-jpeg@outlook.fr>
2025-09-24 06:26:52 +0200

    update submodule

--- tools.lua
+++ tools.lua
@@ -137,7 +137,8 @@ function self.build_kconfig(source, cflags, cppflags)
         end
 
         os.execute("cp ../../config .config")
-        os.execute("KCONFIG_NOTIMESTAMP=1 " .. self.get_flags(cflags, cppflags) .. " make" .. system.get_make_jobs())
+        os.execute("make olddefconfig")
+        os.execute("KCONFIG_NOTIMESTAMP=1 " .. self.get_flags(cflags, cppflags) .. " make ARCH=x86_64" .. system.get_make_jobs())
     end
 end
 
