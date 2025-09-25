c352a25cf4d469a3c9e3e67e0a93e48f0a04f410
Stilic <lateman-jpeg@outlook.fr>
2025-09-25 23:45:25 +0200

    oops

--- tools.lua
+++ tools.lua
@@ -134,7 +134,7 @@ function self.build_python(source, env)
             lfs.chdir(source)
         end
 
-        os.execute(env .. "python -m gpep517 build-wheel --output-fd 1 --wheel-dir .dist")
+        os.execute(env .. "python -m gpep517 build-wheel --output-fd 1 --wheel-dir dist")
     end
 end
 
