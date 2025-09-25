ed1de799dc97310de5aa5709c76c6f56a675e9de
Stilic <lateman-jpeg@outlook.fr>
2025-09-25 05:38:50 +0000

    Update build_disk.sh

--- neld/build_disk.sh
+++ neld/build_disk.sh
@@ -5,7 +5,8 @@ if ! [ $(id -u) = 0 ]; then
 fi
 truncate -s 100G disk
 mkfs.ext4 -F disk
-mount disk /mnt
-mkdir -p /mnt/home/root
-find .cache ! -name busybox-static* -exec cp -t /mnt {} +
-umount /mnt
+mkdir mnt
+mount disk mnt
+mkdir -p mnt/home/root
+find .cache ! -name busybox-static* -exec cp -t mnt {} +
+umount mnt
