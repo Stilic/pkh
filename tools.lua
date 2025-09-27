1d8bb2369b38e02e0c44230aa76a25d7d70bb478
Stilic <lateman-jpeg@outlook.fr>
2025-09-27 08:07:43 +0200

    update submodule

--- tools.lua
+++ tools.lua
@@ -64,7 +64,7 @@ function self.build_autotools(prefix, options, source, cflags, cppflags)
             lfs.chdir(source)
         end
 
-        os.execute("LIBTOOL=slibtool autoreconf -vif")
+        os.execute("autoreconf -vif")
         self.build_gnu_configure(prefix, options, "", cflags, cppflags)()
     end
 end
