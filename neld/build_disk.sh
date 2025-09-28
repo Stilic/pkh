d54e5df9925df0ab7dc666c7a99e2ae5c40ef6b3
Stilic <lateman-jpeg@outlook.fr>
2025-09-28 18:56:27 +0200

    fix

--- neld/build_disk.sh
+++ neld/build_disk.sh
@@ -3,13 +3,19 @@ if ! [ $(id -u) = 0 ]; then
    echo Please run this script as root.
    exit 1
 fi
+
 cd .build/work
+
 truncate -s 100G disk
 mkfs.ext4 -F disk
+
 mkdir mnt
 mount disk mnt
 cd mnt
+
 mkdir usr root home
 cp ../rootfs.sqsh .
 cp ../../*.sqsh usr
+
+cd ..
 umount mnt
--- neld/build_ramdisk.sh
+++ neld/build_ramdisk.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 cd .build/work
+
 rm -rf ramdisk
 mkdir ramdisk
 cd ramdisk
+
 mkdir dev proc mnt ro root etc work
+
 cp -ra ../ram_root/* .
 cp -a ../../../ramdisk_init.sh init
+
 find . -print | cpio -o --format=newc | gzip -9 > ../ramdisk.cpio.gz
