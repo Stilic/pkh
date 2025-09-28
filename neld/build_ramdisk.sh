d41d93090b7add0106c027b10e8247b0e49a0b8f
Stilic <lateman-jpeg@outlook.fr>
2025-09-28 18:15:00 +0200

    fix

--- neld/build_ramdisk.sh
+++ neld/build_ramdisk.sh
@@ -4,6 +4,6 @@ rm -rf ramdisk
 mkdir ramdisk
 cd ramdisk
 mkdir -p dev proc mnt root etc work ro/sys ro/dev ro/run ro/proc ro/var ro/tmp ro/usr/lib64 ro/root ro/home ro/etc
-cp -a ram_root/* .
-cp -a ../../ramdisk_init.sh init
-find . -print | cpio -o --format=newc | gzip -9 > ramdisk.cpio.gz
+cp -ra ../ram_root/* .
+cp -a ../../../ramdisk_init.sh init
+find . -print | cpio -o --format=newc | gzip -9 > ../ramdisk.cpio.gz
