19e8b3dd7feeca0cb14b0dca0787b482e0a151db
Stilic <lateman-jpeg@outlook.fr>
2025-09-29 07:39:59 +0200

    update submodule

--- tools.lua
+++ tools.lua
@@ -6,7 +6,6 @@ local self = {}
 self.DEFAULT_CFLAGS = "-O2 -fPIC"
 self.DEFAULT_CPPFLAGS = self.DEFAULT_CFLAGS
 
--- TODO: remove CC
 function self.get_flags(cflags, cppflags)
     return 'CC=gcc CPATH=/usr/include CFLAGS="' ..
         self.DEFAULT_CFLAGS ..
@@ -14,10 +13,7 @@ function self.get_flags(cflags, cppflags)
         '" CPPFLAGS="' .. self.DEFAULT_CPPFLAGS .. (cppflags and (" " .. cppflags) or "") .. '"'
 end
 
-function self.make(prefix, options, cflags, cppflags, configure)
-    if not prefix then
-        prefix = "/usr"
-    end
+function self.make(options, cflags, cppflags, configure)
     if options then
         options = " " .. options
     else
@@ -28,16 +24,16 @@ function self.make(prefix, options, cflags, cppflags, configure)
     end
 
     if lfs.attributes(configure) then
-        os.execute(self.get_flags(cflags, cppflags) .. " ./" .. configure .. " --prefix=" .. prefix .. options)
+        os.execute(self.get_flags(cflags, cppflags) .. " ./" .. configure .. " --prefix=" .. options)
         os.execute("CPATH=/usr/include make" .. system.get_make_jobs())
     else
-        os.execute(self.get_flags(cflags, cppflags) .. " make PREFIX=" .. prefix .. system.get_make_jobs() .. options)
+        os.execute(self.get_flags(cflags, cppflags) .. " make" .. system.get_make_jobs() .. options)
     end
 end
 
 -- build templates
 
-function self.build_gnu_configure(prefix, options, source, cflags, cppflags, configure)
+function self.build_gnu_configure(options, source, cflags, cppflags, configure)
     if not source then
         source = "source"
     end
@@ -47,14 +43,14 @@ function self.build_gnu_configure(prefix, options, source, cflags, cppflags, con
             lfs.chdir(source)
         end
 
-        self.make(prefix, options, cflags, cppflags, configure)
+        self.make(options, cflags, cppflags, configure)
 
         lfs.mkdir("_install")
         os.execute('make install DESTDIR="' .. lfs.currentdir() .. '/_install"')
     end
 end
 
-function self.build_autotools(prefix, options, source, cflags, cppflags)
+function self.build_autotools(options, source, cflags, cppflags)
     if not source then
         source = "source"
     end
@@ -65,14 +61,11 @@ function self.build_autotools(prefix, options, source, cflags, cppflags)
         end
 
         os.execute("autoreconf -vif")
-        self.build_gnu_configure(prefix, options, "", cflags, cppflags)()
+        self.build_gnu_configure(options, "", cflags, cppflags)()
     end
 end
 
-function self.build_meson(prefix, options, source, cflags, cppflags)
-    if not prefix then
-        prefix = "/usr"
-    end
+function self.build_meson(options, source, cflags, cppflags)
     if options then
         options = " " .. options
     else
@@ -82,17 +75,13 @@ function self.build_meson(prefix, options, source, cflags, cppflags)
         source = "source"
     end
 
-    local libdir = "lib"
-    if prefix ~= "/" then
-        libdir = "/" .. libdir
-    end
     return function()
         if source ~= "" then
             lfs.chdir(source)
         end
 
         os.execute(self.get_flags(cflags, cppflags) ..
-            " meson setup build --buildtype=release --libdir=" .. prefix .. libdir .. " --prefix=" .. prefix .. options)
+            " meson setup build --buildtype=release --prefix=/ --libdir=/lib" .. options)
         os.execute("meson compile -C build")
         os.execute('DESTDIR="' .. lfs.currentdir() .. '/_install" meson install -C build')
     end
