d197b4c07e3e5f3332e67e7aefbce755d9cff88d
Stilic <lateman-jpeg@outlook.fr>
2025-10-05 21:33:46 +0200

    ramdisk: mount /dev for dinit

--- neld/ramdisk_init.sh
+++ neld/ramdisk_init.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
-mount -t devtmpfs none /dev
-mount -t proc none /proc
+mount -t devtmpfs tmpfs /dev
+mkdir -p /dev/pts /dev/shm
+mount -t tmpfs -o nodev,nosuid tmpfs /dev/shm
+mount -t devpts -o gid=5 devpts /dev/pts
+mount -t proc proc /proc
 
 mount $(sed -e 's/^.*root=//' -e 's/ .*$//' /proc/cmdline) /mnt
 mount /mnt/rootfs.sqsh /root
-
-#for file in /mnt/usr/*; do 
-#    if [[ "$file" == *.sqsh ]]
-#    then
-#        mount_dir="/$(basename $file | cut -d "," -f 1)"
-#        mkdir $mount_dir
-#        mount $file $mount_dir
-#        lower_dirs="$mount_dir:$lower_dirs"
-#    fi
-#done
-
-#mount -t overlay overlay -o lowerdir=${lower_dirs%?} /root/usr
-
 mount --bind /mnt/home /root/home
 mount --bind /mnt/root /root/root
 mount -t overlay overlay -o lowerdir=/root/etc,upperdir=/etc,workdir=/work /root/etc
 
-#mount --bind /root/usr/lib /root/usr/lib64
+for file in /mnt/usr/*; do 
+   if [[ "$file" == *.sqsh ]]
+   then
+       mount_dir="/$(basename $file | cut -d "," -f 1)"
+       mkdir $mount_dir
+       mount $file $mount_dir
+       lower_dirs="$mount_dir:$lower_dirs"
+   fi
+done
+mount -t overlay overlay -o lowerdir=${lower_dirs%?} /root/usr
 
-mount -t tmpfs var /root/var
-mkdir /root/var/log /root/var/run /root/var/db
+# mount --bind /root/usr/lib /root/usr/lib64
 
-mount -t tmpfs tmp /root/tmp
-
-umount /dev
+mount --move /dev /root/dev
 umount /proc
 
 exec switch_root /root /bin/bash
