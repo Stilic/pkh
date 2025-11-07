98d8732d7bf5d6d073e03e60c1c29ae292adda6f
Stilic <lateman-jpeg@outlook.fr>
2025-11-07 20:31:25 +0100

    Update build.lua

--- build.lua
+++ build.lua
@@ -4,8 +4,9 @@ pcall(require, "luarocks.loader")
 local lfs = require "lfs"
 local pkh = require "main"
 
-for file in lfs.dir("pickle-linux") do
-    if file:sub(1, 1) ~= "." and lfs.attributes(file).mode == "directory" then
+local repository = "pickle-linux/"
+for file in lfs.dir(repository) do
+    if file:sub(1, 1) ~= "." and lfs.attributes(repository .. file).mode == "directory" then
         pkh.build(file)
     end
 end
