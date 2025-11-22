185437a3bd771539d860468aebd2600ca70813a8
Stilic <lateman-jpeg@outlook.fr>
2025-11-22 02:52:29 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -173,8 +173,8 @@ function self.build(name, skip_dependencies)
     end
     -- TODO: remove the gcc libexec workaround
     local command =
-        "bwrap --unshare-ipc --unshare-pid --unshare-net --unshare-uts --unshare-cgroup-try --clearenv --setenv PATH /usr/libexec/gcc/x86_64-pc-linux-musl/14.2.0:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --chdir /root --ro-bind / " ..
-        root_path .. " --dev /dev --tmpfs /tmp " ..
+        "bwrap --unshare-ipc --unshare-pid --unshare-net --unshare-uts --unshare-cgroup-try --clearenv --setenv PATH /usr/libexec/gcc/x86_64-pc-linux-musl/14.2.0:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --chdir /root --ro-bind " ..
+        root_path .. " / --dev /dev --tmpfs /tmp " ..
         "--bind " .. cwd .. " /root --bind " .. build_path .. " /root/" .. build_suffix ..
         " /bin/lua untrusted_build.lua " .. name .. rebuild_option
     print(command)
