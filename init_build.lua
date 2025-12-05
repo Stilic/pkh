8f2ba7ff81d2a01c51cc75f58a5be471aea0b473
Stilic <lateman-jpeg@outlook.fr>
2025-12-05 02:44:10 +0100

    Update init_build.lua

--- init_build.lua
+++ init_build.lua
@@ -12,32 +12,30 @@ local ROOTFS_CACHE = BASE .. ".rootfs/"
 local BUILD_CACHE = BASE .. ".build/"
 
 -- TODO: variant support?
-local function build_and_copy(package)
-    pkh.build(package)
+local function copy(package, path)
     os.execute("cp pickle-linux/" ..
-        package .. "/.build/" .. tools.get_file(package, pkg(package).version) .. " " .. ROOTFS_CACHE)
+        package .. "/.build/" .. tools.get_file(package, pkg(package).version) .. " " .. path)
 end
-
--- STAGE 1: distro toolchain bootstrap
 
 os.execute("rm -rf " .. ROOTFS_CACHE)
 lfs.mkdir(ROOTFS_CACHE)
 
-for _, package in ipairs(config.development) do
-    pkh.build(package)
-end
 for _, package in ipairs(config.bootstrap) do
-    build_and_copy(package)
+    pkh.build(package)
+    copy(package, ROOTFS_CACHE)
 end
 
 os.execute("./pack_rootfs.sh")
-
--- STAGE 2: development environment compilation
 
 os.execute("rm -rf " .. BUILD_CACHE)
 lfs.mkdir(BUILD_CACHE)
 lfs.mkdir(BUILD_CACHE .. "work")
 
 os.execute("cp " .. ROOTFS_CACHE .. "work/rootfs.sqsh " .. BUILD_CACHE .. "work")
+
+for _, package in ipairs(config.development) do
+    pkh.build(package)
+    copy(package, BUILD_CACHE)
+end
 
 pkh.close()
