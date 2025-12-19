dfc89d279148f02197131821d5f763f42bd08fcb
Stilic <lateman-jpeg@outlook.fr>
2025-12-19 23:14:12 +0100

    wip: stage3

---
+++ stage3.lua
@@ -0,0 +1,15 @@
+stage = 3
+require "global"
+pcall(require, "luarocks.loader")
+
+local pkh = require "main"
+local config = require "neld.config"
+
+for _, package in ipairs(config.bootstrap) do
+    pkh.build(package)
+end
+for _, package in ipairs(config.rootfs) do
+    pkh.build(package)
+end
+
+pkh.close()
