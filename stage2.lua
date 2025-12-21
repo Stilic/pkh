0f76d2ac58b8e57116e5a2d409bd6172945c88ee
Stilic <lateman-jpeg@outlook.fr>
2025-12-21 13:06:53 +0100

    wip: stage rework

--- stage2.lua
+++ stage2.lua
@@ -2,66 +2,16 @@ stage = 2
 require "global"
 pcall(require, "luarocks.loader")
 
-local tools = require "tools"
-local config = require "neld.config"
+local stageutils = require "stageutils"
 local pkh = require "main"
 pkh.init()
 
-local BASE = "neld/"
-local ROOTFS_CACHE = BASE .. ".rootfs/"
-local BUILD_CACHE = BASE .. ".build/"
-
-local copied_packages = {}
-local function copy(name, path)
-    local package
-
-    local i = name:find("%.")
-    if i then
-        package = pkg(name:sub(1, i - 1))
-    else
-        package = pkg(name)
-    end
-
-    if copied_packages[name] then
-        return
-    else
-        copied_packages[name] = true
-    end
-
-    os.execute("cp pickle-linux/" ..
-        package.name .. "/.stage2/" .. tools.get_file(name, package.version) .. " " .. path)
-
-    if package.dependencies then
-        for _, dep in ipairs(package.dependencies) do
-            copy(dep.name, path)
-        end
-    end
-end
-
+pkh.build("musl")
 pkh.build("gcc")
 
-copy("gcc.libs", ROOTFS_CACHE)
-copy("gcc", BUILD_CACHE)
-
-os.execute("./pack_rootfs.sh 2")
-
-pkh.close()
-
---------------------------------
-
-pkh.init()
-
-for _, package in ipairs(config.development) do
-    if package ~= "gcc" then
-        pkh.build(package)
-        copy(package, BUILD_CACHE)
-    end
-end
-
-for _, package in ipairs(config.bootstrap) do
-    pkh.build(package)
-    copy(package, ROOTFS_CACHE)
-end
+stageutils.copy("musl", stageutils.ROOTFS_CACHE)
+stageutils.copy("gcc.libs", stageutils.ROOTFS_CACHE)
+stageutils.copy("gcc", stageutils.BUILD_CACHE)
 
 os.execute("./pack_rootfs.sh 2")
 
--- stage3.lua
+++ stage3.lua
@@ -3,17 +3,22 @@ require "global"
 pcall(require, "luarocks.loader")
 
 local config = require "neld.config"
+local stageutils = require "stageutils"
 local pkh = require "main"
 pkh.init()
 
 for _, package in ipairs(config.bootstrap) do
     pkh.build(package)
+    stageutils.copy(package, stageutils.ROOTFS_CACHE)
 end
-for _, package in ipairs(config.rootfs) do
-    pkh.build(package)
-end
+
 for _, package in ipairs(config.development) do
-    pkh.build(package)
+    if package ~= "gcc" then
+        pkh.build(package)
+        stageutils.copy(package, stageutils.BUILD_CACHE)
+    end
 end
+
+os.execute("./pack_rootfs.sh 2")
 
 pkh.close()
