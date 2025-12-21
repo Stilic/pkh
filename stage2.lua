6461e4b4d20f2867bb30425f9fd02aa651efda99
Stilic <lateman-jpeg@outlook.fr>
2025-12-21 13:10:25 +0100

    Update stage2.lua

--- stage2.lua
+++ stage2.lua
@@ -6,12 +6,10 @@ local stageutils = require "stageutils"
 local pkh = require "main"
 pkh.init()
 
-pkh.build("musl")
 pkh.build("gcc")
 
-stageutils.copy("musl", stageutils.ROOTFS_CACHE)
-stageutils.copy("gcc.libs", stageutils.ROOTFS_CACHE)
 stageutils.copy("gcc", stageutils.BUILD_CACHE)
+stageutils.copy("gcc.libs", stageutils.ROOTFS_CACHE)
 
 os.execute("./pack_rootfs.sh 2")
 
