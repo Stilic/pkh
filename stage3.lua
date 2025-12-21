c480e001cbb1778ff52eeabc5c19a6693c1f4d12
Stilic <lateman-jpeg@outlook.fr>
2025-12-21 17:22:10 +0100

    last time i promise

--- stage3.lua
+++ stage3.lua
@@ -6,27 +6,25 @@ local config = require "neld.config"
 local stageutils = require "stageutils"
 local pkh = require "main"
 
-pkh.build("bubblewrap")
+for _, package in ipairs(config.bootstrap) do
+    pkh.build(package)
+end
+for _, package in ipairs(config.development) do
+    -- skip building the compiler since it hardly impacts the process
+    if package ~= "gcc" then
+        pkh.build(package)
+    end
+end
 
--- for _, package in ipairs(config.bootstrap) do
---     pkh.build(package)
--- end
--- for _, package in ipairs(config.development) do
---     -- skip building the compiler since it hardly impacts the process
---     if package ~= "gcc" then
---         pkh.build(package)
---     end
--- end
+for _, package in ipairs(config.bootstrap) do
+    stageutils.copy(package, stageutils.ROOTFS_CACHE)
+end
+for _, package in ipairs(config.development) do
+    if package ~= "gcc" then
+        stageutils.copy(package, stageutils.BUILD_CACHE)
+    end
+end
 
--- for _, package in ipairs(config.bootstrap) do
---     stageutils.copy(package, stageutils.ROOTFS_CACHE)
--- end
--- for _, package in ipairs(config.development) do
---     if package ~= "gcc" then
---         stageutils.copy(package, stageutils.BUILD_CACHE)
---     end
--- end
-
--- os.execute("./pack_rootfs.sh 2")
+os.execute("./pack_rootfs.sh 2")
 
 pkh.close()
